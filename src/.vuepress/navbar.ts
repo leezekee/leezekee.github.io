89bcde0bb73677eb3527a485f92ef9fc107cebae
public <841361034@qq.com>
2023-06-26 14:36:27 +0800

    initial push

---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,10 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  {
+    text: "博客介绍",
+    icon: "bookmark",
+    link: "helloWorld"
+  }
+]);
---
+++ src/intro.md
@@ -0,0 +1,8 @@
+---
+icon: circle-info
+cover: /assets/images/cover3.jpg
+---
+
+# 个人介绍
+
+一个在读的大烂王
