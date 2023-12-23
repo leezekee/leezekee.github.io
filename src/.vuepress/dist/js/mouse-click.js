805f3e917c199799b65e49cfdf83a7c76e3df218
public <841361034@qq.com>
2023-12-23 20:48:16 +0800

    fix bug

--- src/.vuepress/dist/js/mouse-click.js
+++ src/.vuepress/dist/js/mouse-click.js
@@ -4,41 +4,54 @@ function getRandom(max, min) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function clickHandler(e) {
+    console.log('e', e);
+    e.stopPropagation();
+    var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+    var $i = $("<span/>").text(a[a_idx]);
+    a_idx = (a_idx + 1) % a.length;
+    var x = e.pageX,
+        y = e.pageY;
+    $i.css({
+        "z-index": 99999999999999,
+        "top": y - 20,
+        "left": x,
+        "position": "absolute",
+        "font-weight": "bold",
+        "color": `rgb(${getRandom(255, 0)},${getRandom(255, 0)},${getRandom(255, 0)})`,
+        "user-select": 'none',
+        "cursor": 'default',
+        "font-family": "'Zhi Mang Xing', cursive",
+        "font-size": "14px"
+    });
+    $("body").append($i);
+    $i.animate({
+        "top": y - 180,
+        "opacity": 0
+    },
+        1500,
+        function () {
+            $i.remove();
+        });
+}
+
+function debounce(fn, wait = 100) {
+    let timer = null;
+    return function (...args) {
+        const context = this;
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.apply(context, args);
+        }, wait);
+    }
+}
+
 (() => { 
     let interval = setInterval(() => {
         if (jQuery != undefined) {
             clearInterval(interval);
             jQuery(document).ready(function ($) {
-                $("body").click(function (e) {
-                    console.log('e', e);
-                    e.stopPropagation();
-                    var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
-                    var $i = $("<span/>").text(a[a_idx]);
-                    a_idx = (a_idx + 1) % a.length;
-                    var x = e.pageX,
-                        y = e.pageY;
-                    $i.css({
-                        "z-index": 99999999999999,
-                        "top": y - 20,
-                        "left": x,
-                        "position": "absolute",
-                        "font-weight": "bold",
-                        "color": `rgb(${getRandom(255, 0)},${getRandom(255, 0)},${getRandom(255, 0)})`,
-                        "user-select": 'none',
-                        "cursor": 'default',
-                        "font-family": "'Zhi Mang Xing', cursive",
-                        "font-size": "14px"
-                    });
-                    $("body").append($i);
-                    $i.animate({
-                        "top": y - 180,
-                        "opacity": 0
-                    },
-                        1500,
-                        function () {
-                            $i.remove();
-                        });
-                });
+                $("body").click(debounce(clickHandler));
             });
         }
     }, 300);
